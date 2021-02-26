c3d9a367e37c2d6958ef9ae4f42511b3164cd053
andy <xuchaozheng@outlook.com>
2021-02-26 11:19:23 +0800

    feature: 优化myNew

--- src/writecode/new.js
+++ src/writecode/new.js
@@ -1,12 +1,19 @@
 function myNew(fn, ...args) {
-    let obj = {};
-    obj.__proto__ = fn.prototype;
-    fn.apply(obj, args);
-    return obj;
+    let instance = {};
+    instance.__proto__ = fn.prototype;
+    const res = fn.apply(instance, args);
+    if (typeof res === "function" || (typeof res === "object" && res !== null)) {
+        return res;
+    }
+    return instance;
 }
 
 function Animal(name) {
     this.name = name;
 }
+Animal.prototype.say = function () {
+    console.log('i am animal')
+}
 let animal = myNew(Animal, 'cat');
 console.log(animal.name)  // cat
+animal.say()
