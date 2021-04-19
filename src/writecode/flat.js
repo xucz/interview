fa069099a2272c0c3b4ce2b3b6160c3bccdcd8ee
andy <xuchaozheng@outlook.com>
2021-04-19 11:50:45 +0800

    fixed: 解决flat边界问题

--- src/writecode/flat.js
+++ src/writecode/flat.js
@@ -1,36 +1,39 @@
 // 参考链接：https://juejin.cn/post/6844904025993773063
 Array.prototype.flat = function (n = Infinity) {
+    n = +n;
+    if (typeof n !== 'number' || isNaN(n)) {
+        return this.slice();
+    }
     // n: 拍平几层
-    let arr = this;
     if (n > 0) {
-        return arr.reduce((pre, next) => {
+        return this.reduce((pre, next) => {
             return pre.concat(Array.isArray(next) ? next.flat(n - 1) : next)
         }, [])
     } else {
-        return arr.slice();
+        return this.slice();
     }
 };
 
-Array.prototype.myFlat2 = function (n = Infinity) {
-    if (Array.isArray(this)) {
-        let arr = [];
-        if (n <= 0) {
-            return this;
-        }
-        this.forEach((item) => {
-            if (Array.isArray(item)) {
-                arr.push(...item.myFlat2(n - 1))
-            } else {
-                arr.push(item)
-            }
-        })
-    } else {
-        throw new Error('类型错误')
-    }
-}
+// Array.prototype.myFlat2 = function (n = Infinity) {
+//     if (Array.isArray(this)) {
+//         let arr = [];
+//         if (n <= 0) {
+//             return this;
+//         }
+//         this.forEach((item) => {
+//             if (Array.isArray(item)) {
+//                 arr.push(...item.myFlat2(n - 1))
+//             } else {
+//                 arr.push(item)
+//             }
+//         })
+//     } else {
+//         throw new Error('类型错误')
+//     }
+// }
 
 
 
 let a = [1,2,[3,4,,,[5, 6]]]
-console.log(a.flat(3))
+console.log(a.flat('1-'))
 // reduce 不会调用4 - 5 之间的空部分
